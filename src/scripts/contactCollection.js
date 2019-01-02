a8e1086facc42a779765bc627b881fced4129348
Hannah Neal <hannahmneal@gmail.com>
2019-01-02 00:17:35 -0600

    finished boilerplate, added styling elements, commented instructions and import statements

---
+++ src/scripts/contactCollection.js
@@ -0,0 +1,9 @@
+//--------------------------------- INSTRUCTIONS --------------------------------------------
+// Loads existing contacts from storage and saves new ones. Each new contact should have an auto-generated identifier.
+//-----------------------------------------------------------------------------
+
+//--------------------------------- NOTES --------------------------------------------
+// Interacts with database.json
+//-----------------------------------------------------------------------------
+
+console.log("contactCollection.js");
---
+++ src/scripts/contactList.js
@@ -0,0 +1,11 @@
+//---------------------------- INSTRUCTIONS -------------------------------------------------
+// This component displays all contacts. It should import contact and contactCollection.
+//-----------------------------------------------------------------------------
+
+//------------------------------- NOTES ----------------------------------------------
+
+//-----------------------------------------------------------------------------
+import contact from "./contact"
+import contactCollection from "./contactCollection"
+
+console.log("contactList.js");
